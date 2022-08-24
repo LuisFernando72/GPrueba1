7c9d67953b914928141443ae1c2d126f75e1dcdf
LuisFernando72 <lpaxelc@miumg.edu.gt>
2022-08-24 01:00:24 -0600

    commit_g1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function mostrarContra() {
+    var x = document.getElementById("contra");
+    var y = document.getElementById("mostrar");
+    var z = document.getElementById("ocultar");
+  
+    if (x.type == "password") {
+      x.type = "text";
+      y.style.display = "block";
+      z.style.display = "none";
+    }else{
+      x.type = "password";
+      y.style.display = "none";
+      z.style.display = "block";
+    }
+  }
+  
